f9966c9eb21f610063e02e62c2fbab09198cc3a7
Vani Haripriya Mudadla <vmudadla@redhat.com>
2022-08-16 14:28:18 -0500

    Updated DataInputSchemaDeserializer
    Signed-off-by: Vani Haripriya Mudadla <vmudadla@redhat.com>

--- api/src/main/java/io/serverlessworkflow/api/deserializers/DataInputSchemaDeserializer.java
+++ api/src/main/java/io/serverlessworkflow/api/deserializers/DataInputSchemaDeserializer.java
@@ -18,14 +18,10 @@ package io.serverlessworkflow.api.deserializers;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.serverlessworkflow.api.interfaces.WorkflowPropertySource;
-import io.serverlessworkflow.api.utils.Utils;
 import io.serverlessworkflow.api.workflow.DataInputSchema;
 import java.io.IOException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,39 +54,12 @@ public class DataInputSchemaDeserializer extends StdDeserializer<DataInputSchema
     if (node.isObject() && node.get("schema").isObject() && !node.get("schema").isEmpty()) {
       schemaDefinition = node.get("schema");
       dataInputSchema.setFailOnValidationErrors(node.get("failOnValidationErrors").asBoolean());
+      dataInputSchema.setSchemaDef(schemaDefinition);
     } else {
       String schemaFileDef = node.isObject() ? node.get("schema").asText() : node.asText();
       dataInputSchema.setFailOnValidationErrors(true);
       dataInputSchema.setRefValue(schemaFileDef);
-      String schemaFileContent = Utils.getResourceFileAsString(schemaFileDef);
-      JsonNode schemaRefNode;
-      ObjectMapper jsonWriter = new ObjectMapper();
-      if (schemaFileContent != null && schemaFileContent.trim().length() > 0) {
-        // if its a yaml def convert to json first
-        if (!schemaFileContent.trim().startsWith("{")) {
-          // convert yaml to json to validate
-          ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
-          Object schemaObj = yamlReader.readValue(schemaFileContent, Object.class);
-          schemaRefNode =
-              jsonWriter.readTree(
-                  new JSONObject(jsonWriter.writeValueAsString(schemaObj)).toString());
-        } else {
-          schemaRefNode = jsonWriter.readTree(new JSONObject(schemaFileContent).toString());
-        }
-
-        JsonNode schemaRef = schemaRefNode.get("schema");
-        if (schemaRef != null) {
-          schemaDefinition = schemaRef;
-        } else {
-          logger.error(
-              "Unable to find schema definitions in reference file: {}", schemaFileContent);
-        }
-
-      } else {
-        logger.error("Unable to load schema defs reference file: {}", schemaFileContent);
-      }
     }
-    dataInputSchema.setSchemaDef(schemaDefinition);
     return dataInputSchema;
   }
 }
