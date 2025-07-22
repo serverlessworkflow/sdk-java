4a0a2a775bfed8c2afd53455cb0a2692eebd89ed
Dmitrii Tikhomirov <chani.liet@gmail.com>
2025-07-22 14:41:18 -0700

    initial

---
+++ experimental/ai/impl/src/main/java/io/serverlessworkflow/impl/executors/ai/AIChatModelCallExecutor.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2020-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.serverlessworkflow.impl.executors.ai;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.output.FinishReason;
+import dev.langchain4j.model.output.TokenUsage;
+import io.serverlessworkflow.api.types.TaskBase;
+import io.serverlessworkflow.api.types.ai.CallAIChatModel;
+import io.serverlessworkflow.impl.TaskContext;
+import io.serverlessworkflow.impl.WorkflowApplication;
+import io.serverlessworkflow.impl.WorkflowContext;
+import io.serverlessworkflow.impl.WorkflowModel;
+import io.serverlessworkflow.impl.WorkflowModelFactory;
+import io.serverlessworkflow.impl.executors.CallableTask;
+import io.serverlessworkflow.impl.resources.ResourceLoader;
+import io.serverlessworkflow.impl.services.ChatModelService;
+
+
+public class AIChatModelCallExecutor implements CallableTask<CallAIChatModel> {
+
+  private static final Pattern VARIABLE_PATTERN = Pattern.compile("\\{\\{\\s*(.+?)\\s*\\}\\}");
+
+  @Override
+  public void init(CallAIChatModel task, WorkflowApplication application, ResourceLoader loader) {
+
+  }
+
+  @Override
+  public CompletableFuture<WorkflowModel> apply(WorkflowContext workflowContext, TaskContext taskContext, WorkflowModel input) {
+    WorkflowModelFactory modelFactory = workflowContext.definition().application().modelFactory();
+    if (taskContext.task() instanceof CallAIChatModel callAIChatModel) {
+      return CompletableFuture.completedFuture(modelFactory.fromAny(doCall(callAIChatModel, input.asJavaObject())));
+    }
+    throw new IllegalArgumentException("AIChatModelCallExecutor can only process CallAIChatModel tasks, but received: " + taskContext.task().getClass().getName());
+  }
+
+  @Override
+  public boolean accept(Class<? extends TaskBase> clazz) {
+    return CallAIChatModel.class.isAssignableFrom(clazz);
+  }
+
+  private Object doCall(CallAIChatModel callAIChatModel, Object javaObject) {
+    validate(callAIChatModel, javaObject);
+    ChatModel chatModel = createChatModel(callAIChatModel);
+    Map<String, Object> substitutions = (Map<String, Object>) javaObject;
+
+    List<ChatMessage> messages = new ArrayList<>();
+
+    if (callAIChatModel.getChatModelRequest().getSystemMessages() != null) {
+      for (String systemMessage : callAIChatModel.getChatModelRequest().getSystemMessages()) {
+        String fixedUserMessage = replaceVariables(systemMessage, substitutions);
+        messages.add(new SystemMessage(fixedUserMessage));
+      }
+    }
+
+    if (callAIChatModel.getChatModelRequest().getUserMessages() != null) {
+      for (String userMessage : callAIChatModel.getChatModelRequest().getUserMessages()) {
+        String fixedUserMessage = replaceVariables(userMessage, substitutions);
+        messages.add(new UserMessage(fixedUserMessage));
+      }
+    }
+
+    return prepareResponse(chatModel.chat(messages), javaObject);
+  }
+
+  private String replaceVariables(String template, Map<String, Object> substitutions) {
+    Set<String> variables = extractVariables(template);
+    for (Map.Entry<String, Object> entry : substitutions.entrySet()) {
+      String variable = entry.getKey();
+      Object value = entry.getValue();
+      if (value != null && variables.contains(variable)) {
+        template = template.replace("{{" + variable + "}}", value.toString());
+      }
+    }
+    return template;
+  }
+
+  private void validate(CallAIChatModel callAIChatModel, Object javaObject) {
+    // TODO
+  }
+
+  private ChatModel createChatModel(CallAIChatModel callAIChatModel) {
+    ChatModelService chatModelService = getAvailableModel();
+    if (chatModelService != null) {
+      return chatModelService.getChatModel(callAIChatModel.getPreferences());
+    }
+    throw new IllegalStateException("No LLM models found. Please ensure that you have the required dependencies in your classpath.");
+  }
+
+  private ChatModelService getAvailableModel() {
+    ServiceLoader<ChatModelService> loader = ServiceLoader.load(ChatModelService.class);
+
+    for (ChatModelService service : loader) {
+      return service;
+    }
+
+    throw new IllegalStateException("No LLM models found. Please ensure that you have the required dependencies in your classpath.");
+  }
+
+  private Map<String, Object> prepareResponse(ChatResponse response, Object javaObject) {
+
+    String id = response.id();
+    String modelName = response.modelName();
+    TokenUsage tokenUsage = response.tokenUsage();
+    FinishReason finishReason = response.finishReason();
+    AiMessage aiMessage = response.aiMessage();
+
+    Map<String, Object> responseMap = (Map<String, Object>) javaObject;
+    if (response.id() != null) {
+      responseMap.put("id", id);
+    }
+
+    if (modelName != null) {
+      responseMap.put("modelName", modelName);
+    }
+
+    if (tokenUsage != null) {
+      responseMap.put("tokenUsage.inputTokenCount", tokenUsage.inputTokenCount());
+      responseMap.put("tokenUsage.outputTokenCount", tokenUsage.outputTokenCount());
+      responseMap.put("tokenUsage.totalTokenCount", tokenUsage.totalTokenCount());
+    }
+
+    if (finishReason != null) {
+      responseMap.put("finishReason", finishReason.name());
+    }
+
+    if (aiMessage != null) {
+      responseMap.put("text", aiMessage.text());
+    }
+
+    return responseMap;
+  }
+
+  private static Set<String> extractVariables(String template) {
+    Set<String> variables = new HashSet<>();
+    Matcher matcher = VARIABLE_PATTERN.matcher(template);
+    while (matcher.find()) {
+      variables.add(matcher.group(1));
+    }
+    return variables;
+  }
+}
---
+++ experimental/types/src/main/java/io/serverlessworkflow/api/types/ai/CallAIChatModel.java
@@ -0,0 +1,440 @@
+/*
+ * Copyright 2020-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.serverlessworkflow.api.types.ai;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import io.serverlessworkflow.api.types.TaskBase;
+
+public class CallAIChatModel extends TaskBase {
+
+  private ChatModelPreferences chatModelPreferences;
+
+  private ChatModelRequest chatModelRequest;
+
+  private CallAIChatModel() {}
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  @Override
+  public String toString() {
+    return "CallAIChatModel{"
+        + "chatModelPreferences="
+        + chatModelPreferences
+        + ", chatModelRequest="
+        + chatModelRequest
+        + '}';
+  }
+
+  public ChatModelPreferences getPreferences() {
+    return chatModelPreferences;
+  }
+
+  public ChatModelRequest getChatModelRequest() {
+    return chatModelRequest;
+  }
+
+  public static class Builder {
+
+    private Builder() {}
+
+    private ChatModelPreferences chatModelPreferences;
+    private ChatModelRequest chatModelRequest;
+
+    public Builder preferences(ChatModelPreferences chatModelPreferences) {
+      this.chatModelPreferences = chatModelPreferences;
+      return this;
+    }
+
+    public Builder request(ChatModelRequest chatModelRequest) {
+      this.chatModelRequest = chatModelRequest;
+      return this;
+    }
+
+    public CallAIChatModel build() {
+      CallAIChatModel callAIChatModel = new CallAIChatModel();
+      callAIChatModel.chatModelPreferences = this.chatModelPreferences;
+      callAIChatModel.chatModelRequest = this.chatModelRequest;
+      return callAIChatModel;
+    }
+  }
+
+  public static class ChatModelPreferences {
+    private String baseUrl;
+    private String apiKey;
+    private String organizationId;
+    private String projectId;
+    private String modelName;
+    private Double temperature;
+    private Double topP;
+    private Integer maxTokens;
+    private Integer maxCompletionTokens;
+    private Double presencePenalty;
+    private Double frequencyPenalty;
+    private String responseFormat;
+    private Boolean strictJsonSchema;
+    private Integer seed;
+    private String user;
+    private Duration timeout;
+    private Integer maxRetries;
+    private Boolean logRequests;
+    private Boolean logResponses;
+
+    private ChatModelPreferences() {}
+
+    public static ChatModelPreferences.Builder builder() {
+      return new ChatModelPreferences.Builder();
+    }
+
+    public String getBaseUrl() {
+      return baseUrl;
+    }
+
+    public String getApiKey() {
+      return apiKey;
+    }
+
+    public String getOrganizationId() {
+      return organizationId;
+    }
+
+    public String getProjectId() {
+      return projectId;
+    }
+
+    public String getModelName() {
+      return modelName;
+    }
+
+    public Double getTemperature() {
+      return temperature;
+    }
+
+    public Double getTopP() {
+      return topP;
+    }
+
+    public Integer getMaxTokens() {
+      return maxTokens;
+    }
+
+    public Integer getMaxCompletionTokens() {
+      return maxCompletionTokens;
+    }
+
+    public Double getPresencePenalty() {
+      return presencePenalty;
+    }
+
+    public Double getFrequencyPenalty() {
+      return frequencyPenalty;
+    }
+
+    public String getResponseFormat() {
+      return responseFormat;
+    }
+
+    public Boolean getStrictJsonSchema() {
+      return strictJsonSchema;
+    }
+
+    public Integer getSeed() {
+      return seed;
+    }
+
+    public String getUser() {
+      return user;
+    }
+
+    public Duration getTimeout() {
+      return timeout;
+    }
+
+    public Integer getMaxRetries() {
+      return maxRetries;
+    }
+
+    public Boolean getLogRequests() {
+      return logRequests;
+    }
+
+    public Boolean getLogResponses() {
+      return logResponses;
+    }
+
+    @Override
+    public String toString() {
+      return "Builder{"
+          + "baseUrl='"
+          + baseUrl
+          + '\''
+          + ", apiKey='"
+          + apiKey
+          + '\''
+          + ", organizationId='"
+          + organizationId
+          + '\''
+          + ", projectId='"
+          + projectId
+          + '\''
+          + ", modelName='"
+          + modelName
+          + '\''
+          + ", temperature="
+          + temperature
+          + ", topP="
+          + topP
+          + ", maxTokens="
+          + maxTokens
+          + ", maxCompletionTokens="
+          + maxCompletionTokens
+          + ", presencePenalty="
+          + presencePenalty
+          + ", frequencyPenalty="
+          + frequencyPenalty
+          + ", responseFormat='"
+          + responseFormat
+          + '\''
+          + ", strictJsonSchema="
+          + strictJsonSchema
+          + ", seed="
+          + seed
+          + ", user='"
+          + user
+          + '\''
+          + ", timeout="
+          + timeout
+          + ", maxRetries="
+          + maxRetries
+          + ", logRequests="
+          + logRequests
+          + ", logResponses="
+          + logResponses
+          + '}';
+    }
+
+    public static class Builder {
+      private String baseUrl;
+      private String apiKey;
+      private String organizationId;
+      private String projectId;
+      private String modelName;
+      private Double temperature;
+      private Double topP;
+      private Integer maxTokens;
+      private Integer maxCompletionTokens;
+      private Double presencePenalty;
+      private Double frequencyPenalty;
+      private String responseFormat;
+      private Boolean strictJsonSchema;
+      private Integer seed;
+      private String user;
+      private Duration timeout;
+      private Integer maxRetries;
+      private Boolean logRequests;
+      private Boolean logResponses;
+
+      public Builder baseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+        return this;
+      }
+
+      public Builder apiKey(String apiKey) {
+        this.apiKey = apiKey;
+        return this;
+      }
+
+      public Builder organizationId(String organizationId) {
+        this.organizationId = organizationId;
+        return this;
+      }
+
+      public Builder projectId(String projectId) {
+        this.projectId = projectId;
+        return this;
+      }
+
+      public Builder modelName(String modelName) {
+        this.modelName = modelName;
+        return this;
+      }
+
+      public Builder temperature(Double temperature) {
+        this.temperature = temperature;
+        return this;
+      }
+
+      public Builder topP(Double topP) {
+        this.topP = topP;
+        return this;
+      }
+
+      public Builder maxTokens(Integer maxTokens) {
+        this.maxTokens = maxTokens;
+        return this;
+      }
+
+      public Builder maxCompletionTokens(Integer maxCompletionTokens) {
+        this.maxCompletionTokens = maxCompletionTokens;
+        return this;
+      }
+
+      public Builder presencePenalty(Double presencePenalty) {
+        this.presencePenalty = presencePenalty;
+        return this;
+      }
+
+      public Builder frequencyPenalty(Double frequencyPenalty) {
+        this.frequencyPenalty = frequencyPenalty;
+        return this;
+      }
+
+      public Builder responseFormat(String responseFormat) {
+        this.responseFormat = responseFormat;
+        return this;
+      }
+
+      public Builder strictJsonSchema(Boolean strictJsonSchema) {
+        this.strictJsonSchema = strictJsonSchema;
+        return this;
+      }
+
+      public Builder seed(Integer seed) {
+        this.seed = seed;
+        return this;
+      }
+
+      public Builder user(String user) {
+        this.user = user;
+        return this;
+      }
+
+      public Builder timeout(Duration timeout) {
+        this.timeout = timeout;
+        return this;
+      }
+
+      public Builder maxRetries(Integer maxRetries) {
+        this.maxRetries = maxRetries;
+        return this;
+      }
+
+      public Builder logRequests(Boolean logRequests) {
+        this.logRequests = logRequests;
+        return this;
+      }
+
+      public Builder logResponses(Boolean logResponses) {
+        this.logResponses = logResponses;
+        return this;
+      }
+
+      public ChatModelPreferences build() {
+        ChatModelPreferences preferences = new ChatModelPreferences();
+        preferences.baseUrl = this.baseUrl;
+        preferences.apiKey = this.apiKey;
+        preferences.organizationId = this.organizationId;
+        preferences.projectId = this.projectId;
+        preferences.modelName = this.modelName;
+        preferences.temperature = this.temperature;
+        preferences.topP = this.topP;
+        preferences.maxTokens = this.maxTokens;
+        preferences.maxCompletionTokens = this.maxCompletionTokens;
+        preferences.presencePenalty = this.presencePenalty;
+        preferences.frequencyPenalty = this.frequencyPenalty;
+        preferences.responseFormat = this.responseFormat;
+        preferences.strictJsonSchema = this.strictJsonSchema;
+        preferences.seed = this.seed;
+        preferences.user = this.user;
+        preferences.timeout = this.timeout;
+        preferences.maxRetries = this.maxRetries;
+        preferences.logRequests = this.logRequests;
+        preferences.logResponses = this.logResponses;
+        return preferences;
+      }
+    }
+  }
+
+  public static class ChatModelRequest {
+
+    private List<String> userMessages;
+    private List<String> systemMessages;
+
+    private ChatModelRequest() {}
+
+    public List<String> getUserMessages() {
+      return userMessages;
+    }
+
+    public List<String> getSystemMessages() {
+      return systemMessages;
+    }
+
+    public static ChatModelRequest.Builder builder() {
+      return new ChatModelRequest.Builder();
+    }
+
+    @Override
+    public String toString() {
+      return "ChatModelRequest{"
+          + "userMessages="
+          + String.join(",", userMessages)
+          + ", systemMessages="
+          + String.join(",", systemMessages)
+          + '}';
+    }
+
+    public static class Builder {
+      private List<String> userMessages = new ArrayList<>();
+      private List<String> systemMessages = new ArrayList<>();
+
+      private Builder() {}
+
+      public Builder userMessage(String userMessage) {
+        this.userMessages.add(userMessage);
+        return this;
+      }
+
+      public Builder userMessages(Collection<String> userMessages) {
+        this.userMessages.addAll(userMessages);
+        return this;
+      }
+
+      public Builder systemMessage(String systemMessage) {
+        this.systemMessages.add(systemMessage);
+        return this;
+      }
+
+      public Builder systemMessages(Collection<String> systemMessages) {
+        this.systemMessages.addAll(systemMessages);
+        return this;
+      }
+
+      public ChatModelRequest build() {
+        ChatModelRequest request = new ChatModelRequest();
+        request.userMessages = this.userMessages;
+        request.systemMessages = this.systemMessages;
+        return request;
+      }
+    }
+  }
+}
