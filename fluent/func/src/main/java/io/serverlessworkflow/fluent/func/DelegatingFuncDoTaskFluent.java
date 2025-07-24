bd81ad63c7d00a3abbcd5f7ed11fb851e5eb789d
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2025-07-24 17:39:43 -0400

    Improvements on the DSL to make it more extensible (#672)
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

---
+++ fluent/func/src/main/java/io/serverlessworkflow/fluent/func/DelegatingFuncDoTaskFluent.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2020-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.serverlessworkflow.fluent.func;
+
+import io.serverlessworkflow.fluent.spec.HasDelegate;
+import java.util.function.Consumer;
+
+/**
+ * Mixin that implements {@link FuncDoTaskFluent} by forwarding to another instance.
+ *
+ * @param <SELF> concrete builder type
+ */
+public interface DelegatingFuncDoTaskFluent<SELF extends DelegatingFuncDoTaskFluent<SELF>>
+    extends FuncDoTaskFluent<SELF>, HasDelegate {
+
+  @SuppressWarnings("unchecked")
+  default SELF self() {
+    return (SELF) this;
+  }
+
+  @SuppressWarnings("unchecked")
+  private FuncDoTaskFluent<SELF> d() {
+    return (FuncDoTaskFluent<SELF>) this.delegate();
+  }
+
+  @Override
+  default SELF callFn(String name, Consumer<FuncCallTaskBuilder> cfg) {
+    d().callFn(name, cfg);
+    return self();
+  }
+
+  @Override
+  default SELF callFn(Consumer<FuncCallTaskBuilder> cfg) {
+    d().callFn(cfg);
+    return self();
+  }
+
+  @Override
+  default SELF forFn(String name, Consumer<FuncForTaskBuilder> cfg) {
+    d().forFn(name, cfg);
+    return self();
+  }
+
+  @Override
+  default SELF forFn(Consumer<FuncForTaskBuilder> cfg) {
+    d().forFn(cfg);
+    return self();
+  }
+
+  @Override
+  default SELF switchFn(String name, Consumer<FuncSwitchTaskBuilder> cfg) {
+    d().switchFn(name, cfg);
+    return self();
+  }
+
+  @Override
+  default SELF switchFn(Consumer<FuncSwitchTaskBuilder> cfg) {
+    d().switchFn(cfg);
+    return self();
+  }
+
+  @Override
+  default SELF forkFn(String name, Consumer<FuncForkTaskBuilder> cfg) {
+    d().forkFn(name, cfg);
+    return self();
+  }
+
+  @Override
+  default SELF forkFn(Consumer<FuncForkTaskBuilder> cfg) {
+    d().forkFn(cfg);
+    return self();
+  }
+}
--- fluent/func/src/main/java/io/serverlessworkflow/fluent/func/FuncTaskItemListBuilder.java
+++ fluent/func/src/main/java/io/serverlessworkflow/fluent/func/FuncTaskItemListBuilder.java
@@ -18,13 +18,19 @@ package io.serverlessworkflow.fluent.func;
 import io.serverlessworkflow.api.types.Task;
 import io.serverlessworkflow.api.types.TaskItem;
 import io.serverlessworkflow.fluent.spec.BaseTaskItemListBuilder;
+import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
-public class FuncTaskItemListBuilder extends BaseTaskItemListBuilder<FuncTaskItemListBuilder> {
+public class FuncTaskItemListBuilder extends BaseTaskItemListBuilder<FuncTaskItemListBuilder>
+    implements FuncDoTaskFluent<FuncTaskItemListBuilder> {
 
-  FuncTaskItemListBuilder() {
+  public FuncTaskItemListBuilder() {
     super();
+  }
+
+  public FuncTaskItemListBuilder(final List<TaskItem> list) {
+    super(list);
   }
 
   @Override
@@ -37,17 +43,20 @@ public class FuncTaskItemListBuilder extends BaseTaskItemListBuilder<FuncTaskIte
     return new FuncTaskItemListBuilder();
   }
 
-  public FuncTaskItemListBuilder callJava(String name, Consumer<FuncCallTaskBuilder> consumer) {
+  @Override
+  public FuncTaskItemListBuilder callFn(String name, Consumer<FuncCallTaskBuilder> consumer) {
     this.requireNameAndConfig(name, consumer);
     final FuncCallTaskBuilder callTaskJavaBuilder = new FuncCallTaskBuilder();
     consumer.accept(callTaskJavaBuilder);
     return addTaskItem(new TaskItem(name, new Task().withCallTask(callTaskJavaBuilder.build())));
   }
 
-  public FuncTaskItemListBuilder callJava(Consumer<FuncCallTaskBuilder> consumer) {
-    return this.callJava(UUID.randomUUID().toString(), consumer);
+  @Override
+  public FuncTaskItemListBuilder callFn(Consumer<FuncCallTaskBuilder> consumer) {
+    return this.callFn(UUID.randomUUID().toString(), consumer);
   }
 
+  @Override
   public FuncTaskItemListBuilder forFn(String name, Consumer<FuncForTaskBuilder> consumer) {
     this.requireNameAndConfig(name, consumer);
     final FuncForTaskBuilder forTaskJavaBuilder = new FuncForTaskBuilder();
@@ -55,10 +64,12 @@ public class FuncTaskItemListBuilder extends BaseTaskItemListBuilder<FuncTaskIte
     return this.addTaskItem(new TaskItem(name, new Task().withForTask(forTaskJavaBuilder.build())));
   }
 
+  @Override
   public FuncTaskItemListBuilder forFn(Consumer<FuncForTaskBuilder> consumer) {
     return this.forFn(UUID.randomUUID().toString(), consumer);
   }
 
+  @Override
   public FuncTaskItemListBuilder switchFn(String name, Consumer<FuncSwitchTaskBuilder> consumer) {
     this.requireNameAndConfig(name, consumer);
     final FuncSwitchTaskBuilder funcSwitchTaskBuilder = new FuncSwitchTaskBuilder();
@@ -67,7 +78,22 @@ public class FuncTaskItemListBuilder extends BaseTaskItemListBuilder<FuncTaskIte
         new TaskItem(name, new Task().withSwitchTask(funcSwitchTaskBuilder.build())));
   }
 
+  @Override
   public FuncTaskItemListBuilder switchFn(Consumer<FuncSwitchTaskBuilder> consumer) {
     return this.switchFn(UUID.randomUUID().toString(), consumer);
+  }
+
+  @Override
+  public FuncTaskItemListBuilder forkFn(Consumer<FuncForkTaskBuilder> cfg) {
+    return this.forkFn(UUID.randomUUID().toString(), cfg);
+  }
+
+  @Override
+  public FuncTaskItemListBuilder forkFn(String name, Consumer<FuncForkTaskBuilder> cfg) {
+    this.requireNameAndConfig(name, cfg);
+    final FuncForkTaskBuilder forkTaskJavaBuilder = new FuncForkTaskBuilder();
+    cfg.accept(forkTaskJavaBuilder);
+    return this.addTaskItem(
+        new TaskItem(name, new Task().withForkTask(forkTaskJavaBuilder.build())));
   }
 }
