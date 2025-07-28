b11505ba1e56d94ecd69ad65b986bb8cffaf8215
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2025-07-28 19:48:24 -0400

    Introduce Fluent Agent DSL (depends on LangChain4j SNAPSHOT) (#677)
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

---
+++ fluent/agentic/src/main/java/io/serverlessworkflow/fluent/agentic/AgentWorkflowBuilder.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.serverlessworkflow.fluent.agentic;
+
+import io.serverlessworkflow.fluent.spec.BaseWorkflowBuilder;
+import java.util.UUID;
+
+public class AgentWorkflowBuilder
+    extends BaseWorkflowBuilder<
+        AgentWorkflowBuilder, AgentDoTaskBuilder, AgentTaskItemListBuilder> {
+
+  AgentWorkflowBuilder(final String name, final String namespace, final String version) {
+    super(name, namespace, version);
+  }
+
+  public static AgentWorkflowBuilder workflow() {
+    return new AgentWorkflowBuilder(
+        UUID.randomUUID().toString(), DEFAULT_NAMESPACE, DEFAULT_VERSION);
+  }
+
+  public static AgentWorkflowBuilder workflow(String name) {
+    return new AgentWorkflowBuilder(name, DEFAULT_NAMESPACE, DEFAULT_VERSION);
+  }
+
+  public static AgentWorkflowBuilder workflow(String name, String ns) {
+    return new AgentWorkflowBuilder(name, ns, DEFAULT_VERSION);
+  }
+
+  @Override
+  protected AgentDoTaskBuilder newDo() {
+    return new AgentDoTaskBuilder();
+  }
+
+  @Override
+  protected AgentWorkflowBuilder self() {
+    return this;
+  }
+}
---
+++ fluent/agentic/src/test/java/io/serverlessworkflow/fluent/agentic/Agents.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.serverlessworkflow.fluent.agentic;
+
+import dev.langchain4j.agentic.Agent;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import java.util.List;
+
+public interface Agents {
+
+  interface MovieExpert {
+
+    @UserMessage(
+        """
+                                You are a great evening planner.
+                                Propose a list of 3 movies matching the given mood.
+                                The mood is {mood}.
+                                Provide a list with the 3 items and nothing else.
+                                """)
+    @Agent
+    List<String> findMovie(@V("mood") String mood);
+  }
+}
