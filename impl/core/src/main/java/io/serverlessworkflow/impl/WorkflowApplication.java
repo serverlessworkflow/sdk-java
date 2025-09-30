829ab480316580a91825e89a6b27dc0d1d19ae1d
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-09-30 07:46:58 -0300

    fix: use ulid library safe for graalvm native compilation (#817)
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- impl/core/src/main/java/io/serverlessworkflow/impl/WorkflowApplication.java
+++ impl/core/src/main/java/io/serverlessworkflow/impl/WorkflowApplication.java
@@ -15,7 +15,6 @@
  */
 package io.serverlessworkflow.impl;
 
-import com.github.f4b6a3.ulid.UlidCreator;
 import io.serverlessworkflow.api.types.SchemaInline;
 import io.serverlessworkflow.api.types.Workflow;
 import io.serverlessworkflow.impl.events.EventConsumer;
@@ -141,7 +140,7 @@ public class WorkflowApplication implements AutoCloseable {
     private ResourceLoaderFactory resourceLoaderFactory = DefaultResourceLoaderFactory.get();
     private SchemaValidatorFactory schemaValidatorFactory;
     private WorkflowPositionFactory positionFactory = () -> new QueueWorkflowPosition();
-    private WorkflowInstanceIdFactory idFactory = () -> UlidCreator.getMonotonicUlid().toString();
+    private WorkflowInstanceIdFactory idFactory;
     private ExecutorServiceFactory executorFactory = new DefaultExecutorServiceFactory();
     private EventConsumer<?, ?> eventConsumer;
     private Collection<EventPublisher> eventPublishers = new ArrayList<>();
@@ -243,6 +242,9 @@ public class WorkflowApplication implements AutoCloseable {
                       }
                       return inMemory;
                     });
+      }
+      if (idFactory == null) {
+        idFactory = new MonotonicUlidWorkflowInstanceIdFactory();
       }
       return new WorkflowApplication(this);
     }
