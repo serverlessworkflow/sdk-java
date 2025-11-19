d6e4f07d03de4b337686156268ac60173e14fdf7
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2025-11-19 06:36:48 -0500

    no-issue: fix JavaFilterFunctionCallExecutor missing from context (#991)
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

---
+++ experimental/lambda/src/main/java/io/serverlessworkflow/impl/executors/func/JavaFilterFunctionCallExecutor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.serverlessworkflow.impl.executors.func;
+
+import io.serverlessworkflow.api.types.TaskBase;
+import io.serverlessworkflow.api.types.func.CallJava;
+import io.serverlessworkflow.api.types.func.JavaFilterFunction;
+import io.serverlessworkflow.impl.TaskContext;
+import io.serverlessworkflow.impl.WorkflowContext;
+import io.serverlessworkflow.impl.WorkflowDefinition;
+import io.serverlessworkflow.impl.WorkflowModel;
+import io.serverlessworkflow.impl.WorkflowModelFactory;
+import io.serverlessworkflow.impl.executors.CallableTask;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public class JavaFilterFunctionCallExecutor<T, V>
+    implements CallableTask<CallJava.CallJavaFilterFunction<T, V>> {
+
+  private JavaFilterFunction<T, V> function;
+  private Optional<Class<T>> inputClass = Optional.empty();
+
+  @Override
+  public void init(CallJava.CallJavaFilterFunction<T, V> task, WorkflowDefinition definition) {
+    this.function = task.function();
+    this.inputClass = task.inputClass();
+  }
+
+  @Override
+  public CompletableFuture<WorkflowModel> apply(
+      WorkflowContext workflowContext, TaskContext taskContext, WorkflowModel input) {
+    WorkflowModelFactory mf = workflowContext.definition().application().modelFactory();
+    T typedIn = JavaFuncUtils.convertT(input, inputClass);
+    V out = function.apply(typedIn, workflowContext, taskContext);
+    return CompletableFuture.completedFuture(mf.fromAny(input, out));
+  }
+
+  @Override
+  public boolean accept(Class<? extends TaskBase> clazz) {
+    return CallJava.CallJavaFilterFunction.class.isAssignableFrom(clazz);
+  }
+}
