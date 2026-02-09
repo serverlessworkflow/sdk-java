91fe6e52b3646d7dfc1568b0aba4dda342de29fc
fjtirado <ftirados@redhat.com>
2026-02-09 14:37:54 +0100

    [Fix #1141] Support CompletableFuture as return value for Java call
    Signed-off-by: fjtirado <ftirados@redhat.com>

---
+++ experimental/lambda/src/main/java/io/serverlessworkflow/impl/executors/func/JavaConsumerCallExecutorBuilder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.serverlessworkflow.impl.executors.func;
+
+import io.serverlessworkflow.api.types.TaskBase;
+import io.serverlessworkflow.api.types.func.CallJava;
+import io.serverlessworkflow.impl.WorkflowDefinition;
+import io.serverlessworkflow.impl.WorkflowMutablePosition;
+import io.serverlessworkflow.impl.executors.CallableTask;
+import io.serverlessworkflow.impl.executors.CallableTaskBuilder;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class JavaConsumerCallExecutorBuilder<T>
+    implements CallableTaskBuilder<CallJava.CallJavaConsumer<T>> {
+
+  private Consumer<T> consumer;
+  private Optional<Class<T>> inputClass;
+
+  public void init(
+      CallJava.CallJavaConsumer<T> task,
+      WorkflowDefinition definition,
+      WorkflowMutablePosition position) {
+    consumer = task.consumer();
+    inputClass = task.inputClass();
+  }
+
+  @Override
+  public boolean accept(Class<? extends TaskBase> clazz) {
+    return CallJava.CallJavaConsumer.class.isAssignableFrom(clazz);
+  }
+
+  @Override
+  public CallableTask build() {
+    return new JavaConsumerCallExecutor<T>(inputClass, consumer);
+  }
+}
--- experimental/lambda/src/test/java/io/serverless/workflow/impl/executors/func/CallJavaContextFunctionTest.java
+++ experimental/lambda/src/test/java/io/serverless/workflow/impl/executors/func/CallJavaContextFunctionTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.serverless.workflow.impl;
+package io.serverless.workflow.impl.executors.func;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
